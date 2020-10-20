82c56b2d402f12b6ae1d36b2ffa274103fbdcdb3
deficts <diegoar7@live.com>
2020-10-20 16:31:31 -0500

    Reflejo a moneda y nuevo fondo

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 50 * Time.deltaTime);
+    }
+}
